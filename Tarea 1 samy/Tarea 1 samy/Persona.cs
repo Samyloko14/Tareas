d35ffaf68600ad549d3d67a1420a464b4da295cd
Samyloko14 <samyberz@outlook.com>
2016-10-13 19:12:36 -0700

    Tarea 1

---
+++ Tarea 1 samy/Tarea 1 samy/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1_samy
+{
+    public class Persona: Program
+    {
+        string Nombre;
+        int Edad = 0;
+        float Salario = 0;
+
+        
+    }
+}
---
+++ Tarea 1 samy/Tarea 1 samy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1_samy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string linea;
+            string Nombre;
+            int Edad = 0;
+            double Salario = 0;
+            
+
+            Console.WriteLine("Ingrese nombre : ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la edad: ");
+            linea = Console.ReadLine();
+            Edad = int.Parse(linea);
+            Console.WriteLine("ingrese Salario: ");
+            linea = Console.ReadLine();
+            Salario = float.Parse(linea);
+            if (Salario >= 10000)
+            {
+                double iva = Salario * 0.20;
+                double SueldoFinal = Salario - iva;
+                Console.Write("sueldo final: ");
+                Console.Write(SueldoFinal);
+            }
+            else
+            {
+                double iva = Salario * 0.16;
+                double SueldoFinal = Salario - iva;
+                Console.Write("sueldo final: ");
+                Console.Write(SueldoFinal);
+            }
+            Console.ReadKey();
+        }
+    }
+}
